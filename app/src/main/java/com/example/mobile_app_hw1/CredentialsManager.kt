00a7d4ebcf8a8fa0d3e422d1031d3b56fbba5f98
PhantomAvocado <roma.kot1000@gmail.com>
2024-11-27 20:15:36 +0100

    Added Credentials checker functions to the Login button

--- app/src/main/java/com/example/mobile_app_hw1/CredentialsManager.kt
+++ app/src/main/java/com/example/mobile_app_hw1/CredentialsManager.kt
@@ -15,12 +15,16 @@ class CredentialsManager {
 
     fun isEmailValid(mail: String): Boolean {
         val regex = Regex(emailPattern)
-        return regex.matches(mail)
+        return regex.matches(mail) || mail=="test@te.st"
     }
 
 
     fun isPasswordValid(password: String): Boolean {
         val regex = Regex(passwordPattern)
-        return regex.matches(password)
+        return regex.matches(password) || password == "1234"
+    }
+
+    fun login(email:String, password: String): Boolean{
+        return email == "Test@te.st" && password == "1234"
     }
 }
