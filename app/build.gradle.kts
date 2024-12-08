7fd3804ae2f2150689d0f56795416d7c589c84cb
Sofiia <sofiiavoitkova@gmail.com>
2024-12-08 01:50:14 +0100

    Add libs.versions.toml for dependency management

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation ("androidx.fragment:fragment-ktx:1.5.7")
+    implementation ("androidx.fragment:fragment-ktx:1.8.5")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
