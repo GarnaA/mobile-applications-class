dcc5a2263f9a7741618153427d4b4c2fbc60b422
Sofiia <sofiiavoitkova@gmail.com>
2024-12-22 21:43:15 +0100

    Create Recipe RecyclerView list

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -47,4 +48,5 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    implementation ("com.github.bumptech.glide:glide:4.15.1")
 }
--- app/src/main/java/com/example/mobile_app_hw1/MainActivity.kt
+++ app/src/main/java/com/example/mobile_app_hw1/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.mobile_app_hw1
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.DividerItemDecoration
 
-class MainActivity:AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val divider = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+        recyclerView.addItemDecoration(divider)
+
+        val recipes = listOf(
+            Recipe(1, "STEAK", imageUrl = R.drawable.steak),
+            Recipe(2, "KHINKALI", imageUrl = R.drawable.khinkali),
+            Recipe(3, "KHACHAPURI", imageUrl = R.drawable.khachapuri)
+        )
+
+        val adapter = RecipeAdapter(
+            recipes,
+            itemClickListener = { recipe ->
+                Toast.makeText(this, "Clicked: ${recipe.title}", Toast.LENGTH_SHORT).show()
+                Log.d("Item Clicked", "${recipe.id}")
+            },
+            actionClickListener = { recipe, action ->
+                Toast.makeText(this, "$action clicked: ${recipe.title}", Toast.LENGTH_SHORT).show()
+                Log.d("Action Clicked", "$action clicked: ${recipe.id}")
+            }
+        )
+
+        recyclerView.adapter = adapter
     }
 }
